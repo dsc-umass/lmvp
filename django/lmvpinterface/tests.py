e1ccf4526d4d1cccbe886815be3240c942f66e13
jbinvnt <17530522+jbinvnt@users.noreply.github.com>
2020-05-21 23:51:46 -0500

    Autotests finally seem to work now

--- django/lmvpinterface/tests.py
+++ django/lmvpinterface/tests.py
@@ -1,18 +1,23 @@
 from django.test import TestCase, RequestFactory #we may migrate to the DRF test utilities in the future
 #from rest_framework.test import APIRequestFactory #this is provided by DRF for testing
 from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
 from .models import *
 from os import path
+import hashlib
 # Create your tests here.
 class TautologyTestCase(TestCase):
     def test_true(self):
         self.assertEqual(True, True)
 class RetrievalTestCase(TestCase):
     def setUp(self):
+        self.factory = RequestFactory()
+        test_hash = hashlib.sha1()
+        test_hash.update(b'Arbitrary test text')
         Project.objects.create(name="TestProject")
         User.objects.create(username="hpotter", created=timezone.now())
         Commit.objects.create(author=User.objects.get(username="hpotter"), project=Project.objects.get(name="TestProject"),
-        hash=0b1001011010100011010111011001011010011110110110110, created=timezone.now())
+        hash=test_hash.digest(), created=timezone.now())
     def test_retrieve_user(self):
         request = self.factory.get('/users')
         request.user = AnonymousUser() #TODO complete this test
@@ -20,4 +25,4 @@ class RetrievalTestCase(TestCase):
         pass #TODO complete this test
 class StaticFormTestCase(TestCase):
     def test_static_form_present(self):
-        self.assertEqual(os.path.exists('management/commands/genforms.py'))
+        self.assertEqual(path.exists('/django/lmvpinterface/management/commands/genforms.py'), True)
--- runTests.sh
+++ runTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /django
 python3 manage.py test | grep 'FAIL'
-if [[ $? -ne 0 ]]; then
+if [[ $? -eq 0 ]]; then
   exit 1 #if the test output has "FAIL" in it, then exit with error code
 fi
 exit 0 # tests pass
