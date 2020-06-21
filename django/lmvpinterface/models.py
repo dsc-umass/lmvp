5887444dc9d73869e297e3b4231bdf24995d513e
jbinvnt <17530522+jbinvnt@users.noreply.github.com>
2020-06-21 15:04:35 -0500

    Add method to get related name for metrics and properties

--- django/lmvpinterface/models.py
+++ django/lmvpinterface/models.py
@@ -19,22 +19,32 @@ class Commit(models.Model):
     file = models.FileField(blank=True) #django docs suggest using ModelFormWithFileField in views
     def save(self, *args, **kwargs):
         hasher = sha1()
-        hasher.update(self.author.encode('utf-8'))
-        hasher.update(self.project.encode('utf-8'))
-        hasher.update(self.created.encode('utf-8'))
-        if file:
+        hasher.update(self.author.username.encode('utf-8'))
+        hasher.update(self.project.name.encode('utf-8'))
+        hasher.update(str(self.created).encode('utf-8'))
+        if self.file:
             hasher.update(self.file.path.encode('utf-8')) # TODO actually hash the file contents, also
         self.hash = hasher.digest()
         super().save(*args, **kwargs)
 
 class BaseAttribute(models.Model):
+    @staticmethod
+    def commit_related_name(): # see the following: https://stackoverflow.com/questions/42899817/django-orm-override-related-name-of-field-in-child-class #TODO investigate default_related_name
+        return "%(app_label)s_%(class)ss"
     class Meta:
         abstract = True
-    commit = models.ForeignKey(Commit, related_name="%(class)ss", on_delete=models.CASCADE) #now Commit has a field called metrics
+    # see https://stackoverflow.com/questions/41921255/staticmethod-object-is-not-callable
+    commit = models.ForeignKey(Commit, related_name=commit_related_name.__func__(), on_delete=models.CASCADE) #now Commit has a field with this name
     name = models.CharField(max_length=128)
 class Metric(BaseAttribute):
+    @staticmethod
+    def commit_related_name():
+        return "metrics"
     value = models.FloatField()
 class Property(BaseAttribute):
+    @staticmethod
+    def commit_related_name():
+        return "properties"
     value = models.CharField(max_length=128, blank=True)
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
