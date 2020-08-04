1df8041a7fae9c4690dcbd5f734fcbd03ce4b326
Oluwadamilola Adesola <oluwadamilolaadesola@Oluwadamilolas-MacBook-Pro.local>
2020-08-04 11:43:15 +0400

    Remove DS Store files and fixed URL issue

--- django/webapp/urls.py
+++ django/webapp/urls.py
@@ -35,8 +35,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lmvp/', include('lmvpinterface.urls')),
-    path('auth/', include('authentication.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('lmvpinterface.urls')),
+    path('auth/', include('authentication.urls')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
